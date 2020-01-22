d686d8a541ab6a8f81ec30f4230e1af746af10dc
paulvanhecke <p.vanhecke86@gmail.com>
2020-01-22 15:20:43 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+    name='transformers',
+    version=0.1,
+    packages=['transformers'],
+    package_dir={'transformers': 'transformers'},
+    include_package_data=False
+)
